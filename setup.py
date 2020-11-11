1040a9fd1cd1d7a3eac48ae4a1a98dbfcf6d95ae
geraldzakwan <geraldi.dzakwan@gmail.com>
2020-11-11 15:35:27 +0700

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE.md') as f:
+    license = f.read()
+
+setup(
+    name='hafalin',
+    version='0.1.0',
+    description='Sample package for Python-Guide.org',
+    long_description=readme,
+    author='Geraldi Dzakwan; Ari Pratama Zhorifiandi',
+    author_email='geraldi.dzakwan@gmail.com; arizho16@gmail.com',
+    url='https://github.com/geraldzakwan/tajong.ai',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
